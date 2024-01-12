e8cb3983e6770c5cc88c000e8a0115d0f82e4b33
Ko Vial <pawgazer@gmail.com>
2024-01-12 16:40:04 -0500

    wip

--- context/reminders.js
+++ context/reminders.js
@@ -1,4 +1,6 @@
 import { createContext, useContext, useReducer } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { VIEWS } from '../constants';
 
 export const RemindersContext = createContext(VIEWS.main);
@@ -26,6 +28,25 @@ export const RemindersProvider = ({ children }) => {
 export const useReminders = () => useContext(RemindersContext);
 
 export const useRemindersDispatch = () => useContext(RemindersDispatchContext);
+
+const createReminderReducer = async () => {
+  const reminder = { id: 0, label: 'begonia listada', period: 3 };
+  try {
+    const serialized = JSON.stringify(reminder);
+    await AsyncStorage.setItem('reminder1', serialized);
+  } catch (e) {
+    console.log('error from creating reminder', e);
+  }
+};
+
+const getReminderReducer = async () => {
+  try {
+    const reminder = await AsyncStorage.getItem('reminder1');
+    return reminder ? JSON.parse(reminder) : null;
+  } catch (e) {
+    console.log('error from getting reminder');
+  }
+};
 
 // Action is in the form of {type: ACTION_TYPE, payload: DATA /* optional */}
 export const remindersReducer = (reminders, action) => {
