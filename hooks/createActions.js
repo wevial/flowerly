bd06bea27b33d56ae2cacab7917c29887bf715d3
Ko Vial <pawgazer@gmail.com>
2024-01-17 18:00:44 -0500

    Forgot commit createActions hook file. Oops!

---
+++ hooks/createActions.js
@@ -0,0 +1,15 @@
+const createActions = (actionList) => (dispatch) => {
+  const actions = {};
+  Object.entries(actionList).forEach(([key, action]) => {
+    actions[key] = action(dispatch);
+  });
+  return actions;
+};
+
+export const createActionConsts = (actionList = []) =>
+  actionList.reduce((acc, action) => {
+    acc[action] = action;
+    return acc;
+  }, {});
+
+export default createActions;
