1bcd6fbcfd2de6c3ba3dfcb7989e7ac8fb5f1787
Ko Vial <pawgazer@gmail.com>
2024-02-06 15:05:34 -0800

    Remove time component

--- components/Time.js
+++
@@ -1,8 +0,0 @@
-import React, { Text } from "react-native";
-
-const Time = ({ current = true, timeProp }) => {
-  const time = current ? new Date() : timeProp;
-  return <Text>{time.toLocaleDateString()}</Text>;
-};
-
-export default Time;
